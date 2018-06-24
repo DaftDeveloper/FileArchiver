af023042957b698543af4a005e99e423e3c78db4
Tony Katlubeck <tony@fiddlesaw.com>
2018-06-24 10:37:08 -0500

    Initial commit

---
+++ src/FileCopier/FileCopyService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaftDev.FileCopier
+{
+    public class FileCopyService
+    {
+        private readonly string _sourceDir;
+        private readonly string _destDir;
+        private readonly bool _isParallel;
+
+        private readonly IEnumerable<string> _allowedExtensions;
+
+        public FileCopyService(string sourceDir, string destDir, IEnumerable<string> allowedExtensions, bool isParallel = true)
+        {
+            _sourceDir = sourceDir;
+            _destDir = destDir;
+            _allowedExtensions = allowedExtensions;
+            _isParallel = isParallel;
+        }
+
+        public void ProcessMoves()
+        {
+            var allFiles = Directory.EnumerateFiles(_sourceDir, "*.*", SearchOption.AllDirectories);
+
+            if (_allowedExtensions != null && _allowedExtensions.Any())
+                allFiles = allFiles.Where(file => _allowedExtensions.Any(allowedExt => String.Compare(allowedExt, Path.GetExtension(file), true) == 0));
+
+            var fileCount = allFiles.Count();
+            var begin = DateTime.UtcNow;
+
+            Console.WriteLine($"Processing {fileCount} files.");
+
+            if (_isParallel) ProcessFilesParallel(allFiles);
+            else ProcessFiles(allFiles);
+
+            var end = DateTime.UtcNow;
+            var duration = end - begin;
+
+            Console.WriteLine($"Processed {fileCount} files in {Math.Floor(duration.TotalMinutes)} minutes {duration.Seconds} seconds");
+        }
+
+        private void ProcessFiles(IEnumerable<string> files)
+        {
+            foreach (var file in files)
+            {
+                ProcessFile(file);
+            }
+        }
+
+        private void ProcessFilesParallel(IEnumerable<string> files)
+        {
+            Console.WriteLine("Parallel processing enabled.");
+
+            Parallel.ForEach(files, (file) => {
+                ProcessFile(file);
+            });
+        }
+
+        private void ProcessFile(string file)
+        {
+            Console.WriteLine($"Processing file {file}");
+
+            var fileInfo = new FileInfo(file);
+
+            var newFilename = $"{fileInfo.CreationTime.ToString("yyyyMMdd-HHmmss")}_{fileInfo.Name}";
+
+            var newDirectory = Path.Combine(_destDir,
+                fileInfo.CreationTime.ToString("yyyy"),
+                fileInfo.CreationTime.ToString("yyyy-MM-dd"));
+
+            var newPath = Path.Combine(newDirectory, newFilename);
+            Directory.CreateDirectory(newDirectory);
+
+            if (!File.Exists(newPath))
+            {
+                Console.WriteLine($"Copying {file} to {newPath}");
+                File.Copy(file, newPath);
+            }
+            else
+            {
+                Console.WriteLine($"{newPath} already exists");
+            }
+        }
+    }
+}
---
+++ src/FileCopier/Program.cs
@@ -0,0 +1,82 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DaftDev.FileCopier
+{
+    class Program
+    {
+        internal static IConfiguration Configuration { get; set; }
+
+        static void Main(string[] args)
+        {
+            BuildConfiguration();
+
+            var app = CreateApp();
+            app.Execute(args);
+        }
+
+        static private void BuildConfiguration()
+        {
+            Console.WriteLine("Building configuration");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        static private CommandLineApplication CreateApp()
+        {
+            Console.WriteLine("Configuring argument support.");
+
+            var app = new CommandLineApplication();
+            app.HelpOption("-h|--help");
+
+            var inputDirectory = app.Option("-i|--input <DIRECTORY>", "Input Directory", CommandOptionType.SingleValue)
+                .IsRequired()
+                .Accepts(x => x.ExistingDirectory());
+
+            var outputDirectory = app.Option("-o|--output <DIRECTORY>", "Output Directory", CommandOptionType.SingleValue)
+                .IsRequired()
+                .Accepts(x => x.ExistingDirectory());
+
+            var parallelArgument = app.Option("-p|--parallel", "Process files in parallel.", CommandOptionType.NoValue);
+
+            var allowedExtensions = CreateAllowedExtensionsList();
+
+            app.OnExecute(() =>
+            {
+                Console.WriteLine("Starting file copying service.");
+
+                var fileService = new FileCopyService(inputDirectory.Value(), outputDirectory.Value(), allowedExtensions, parallelArgument.HasValue());
+                fileService.ProcessMoves();
+
+                Console.WriteLine("Processing complete, press any key to continue...");
+                Console.ReadKey();
+            });
+
+            return app;
+        }
+
+        static private IEnumerable<string> CreateAllowedExtensionsList()
+        {
+            var allowedExtensions = Configuration.GetSection("AllowedExtensions")
+                .Get<string[]>();
+
+            if (allowedExtensions == null || !allowedExtensions.Any())
+            {
+                Console.WriteLine("Extension list not provided in config, all files will be copied.");
+                allowedExtensions = null;
+            }
+
+            Console.WriteLine($"The following extensions will be processed: {String.Join(", ", allowedExtensions)}");
+
+            return allowedExtensions;
+        }
+    }
+}
